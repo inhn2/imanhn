1f807f106eccbc126499dd93a7a6c098e21e7c9d
inhn2 <137736343+inhn2@users.noreply.github.com>
2024-05-11 00:35:30 +0330

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -1,1 +1,39 @@
+///////////////////////json
+
+const apiUrl = 'https://cdn.tsetmc.com/api/Instrument/GetInstrumentOptionMarketWatch/0';
+
+var opt = [];
+
+ 
+
+fetch(apiUrl)
+
+  .then(response => {
+
+    if (!response.ok) {
+
+      throw new Error('Network response was not ok');
+
+    }
+
+    return response.json();
+
+  })
+
+  .then(data => {
+
+
+for (i=0;i<data.instrumentOptMarketWatch.length;i++){
+
+opt[i] = data.instrumentOptMarketWatch[i];
+ 
+}
+ })
+  .catch(error => {
+
+    console.error('Error:', error);
+
+  });
+  
+///////////////////////////////////json  
  
