38f96c6ee58c26a912712194dcdb56decc5b2cc0
inhn2 <137736343+inhn2@users.noreply.github.com>
2024-05-10 21:14:50 +0330

    Update jstest.js

--- jstest.js
+++ jstest.js
@@ -6,8 +6,6 @@ var b = [];
 var b1 = [];
 var b2 = [];
 var b3 = [];
-var name = 'خودران';
- 
 
 fetch(apiUrl)
 
@@ -23,46 +21,5 @@ fetch(apiUrl)
 
   })
 
-  .then(data => {
 
-
-for (i=0;i<data.instrumentOptMarketWatch.length;i++){
-
-b[i] = data.instrumentOptMarketWatch[i].lval30_UA;
-
-  function checkname(na) {
-  return na == name;
-}
-
-if( name == b[i] ){
-
-b1[i] = i;
-
-}
-
-else{b1[i]=0;}  
- 
- }
- 
-function rz1(z) {
-  return z > 0;
-}
-
-b2 = b1.filter(rz1);
- 
-
-
-for (i=0;i<b2.length;i++){
-
-b3[i] = JSON.stringify(data.instrumentOptMarketWatch[b2[i]].strikePrice);
-
-}
-
- })
-  .catch(error => {
-
-    console.error('Error:', error);
-
-  });
-  
 ///////////////////////////////////json  
