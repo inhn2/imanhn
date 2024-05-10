0204d31cb9dee0b570b4bce602db6aa183b34072
inhn2 <137736343+inhn2@users.noreply.github.com>
2024-05-10 23:29:16 +0330

    Update jsonopt.js

--- jsonopt.js
+++ jsonopt.js
@@ -6,6 +6,7 @@ var b = [];
 var b1 = [];
 var b2 = [];
 var b3 = [];
+var c = [];
 var name = 'خودران';
  
 
@@ -29,6 +30,8 @@ fetch(apiUrl)
 for (i=0;i<data.instrumentOptMarketWatch.length;i++){
 
 b[i] = data.instrumentOptMarketWatch[i].lval30_UA;
+c[i] = data.instrumentOptMarketWatch[i];
+ 
 
   function checkname(na) {
   return na == name;
